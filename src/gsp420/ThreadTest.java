8ffadc6a92579749d0101e7e4ce583699aa4df11
Michael Vaganov <mvaganov@hotmail.com>
2013-09-30 15:06:59 -0700

    * doign a loop in series

--- src/gsp420/ThreadTest.java
+++ src/gsp420/ThreadTest.java
@@ -1,10 +1,12 @@
 package gsp420;
 
 public class ThreadTest {
-	
 	public String message = "message";
 	private int numberOfTimes = 10;
-	
+	public ThreadTest(String m)
+	{
+		message = m;
+	}
 	void DoTheThing()
 	{
 		for(int i = 0; i < numberOfTimes; ++i)
@@ -12,10 +14,11 @@ public class ThreadTest {
 			System.out.println(i+" "+message);
 		}
 	}
-	
 	public static void main(String[] args)
 	{
-		ThreadTest t = new ThreadTest();
+		ThreadTest t = new ThreadTest("a");
+		ThreadTest t2 = new ThreadTest("b");
 		t.DoTheThing();
+		t2.DoTheThing();
 	}
 }
