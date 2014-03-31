8fcc479f332a766a4781e1a3e9cf5e7759386323
mvaganov <mvaganov@hotmail.com>
2014-03-31 14:58:32 -0700

    * cleaning up warning

--- src/gsp420/HelloApplet.java
+++ src/gsp420/HelloApplet.java
@@ -3,6 +3,8 @@ package gsp420;
 import java.applet.Applet;
 
 public class HelloApplet extends Applet {
+	private static final long serialVersionUID = 1L;
+
 	public void init()
 	{
 		add(new Hello("hi.  !"));
