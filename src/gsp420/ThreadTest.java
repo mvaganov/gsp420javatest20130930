bc771aa54eb1a4d0e0f0cf9335163fe1aefb870d
mvaganov <mvaganov@hotmail.com>
2014-03-31 15:28:33 -0700

    * fixed spelling mistakes * refactored functionality into functions that make sense

--- src/gsp420/ThreadTest.java
+++ src/gsp420/ThreadTest.java
@@ -1,70 +1,96 @@
 package gsp420;
 public class ThreadTest implements Runnable {
 	public String message = "message";
+	/** how many times to print the message */
 	private int numberOfTimes = 10;
-	static boolean working = false;
-	/** jsut a global to act like a door. */
+	
+	/** a pretty gross fake semaphore */
+	static volatile boolean working = false;
+	
+	/** just a global to act like a door. */
 	static Object sync = new Object();
-	public ThreadTest(String m)
-	{
+	
+	/** @param m what message to repeat */
+	public ThreadTest(String m) {
 		message = m;
 	}
-	void DoTheThing()
-	{
-//		// really crappy semaphore
-//		while(working) {
-//			try{Thread.sleep(1);}
-//			catch(Exception e){}
-//		}
-//		working = true;
-		synchronized(sync)
-		{
-			// embarassingly parellel
-			for(int i = 0; i < numberOfTimes; ++i)
-			{
-				System.out.print(message);
-			}
-			System.out.println();
+	/** important functionality */
+	void printMessage() {
+		for(int i = 0; i < numberOfTimes; ++i) {
+			System.out.print(message);
 		}
-//		working = false;
+		System.out.println();
 	}
-	public void run(){DoTheThing();}
-	public static void main(String[] args)
-	{
+	/** proxy for run(), using a homebrew semaphore */
+	void DoTheThing() {
+		// really crappy semaphore - it *almost* works. it's orderly *most* of the time
+		while(working) {
+			try{Thread.sleep(1);}catch(Exception e){}
+		}
+		working = true;
+		printMessage();
+		working = false;
+
+	}
+	/** proxy for run, using the correct Java synchronized keyword as a semaphore */
+	void DoTheThingCorrectly() {
+		// using the correct Java solution
+		synchronized(sync) {
+			printMessage();
+		}
+	}
+	/** the method executed by the running thread */
+	public void run() {
+		//DoTheThing();
+		DoTheThingCorrectly();
+	}
+	
+	public static void main(String[] args) {
 		final int count = 10;
+		final boolean usingFunctionalExample = false;
 		Thread[] threads = new Thread[count];
 		ThreadTest[] tests = new ThreadTest[count];
-		// create tests
-		for(int i = 0; i < count; ++i)
-		{
-			tests[i] = new ThreadTest(
-					""+(char)('a'+i));
-		}
-		abstract class temp implements Runnable{
-			ThreadTest t;
-			public temp(ThreadTest t)
-			{
-				this.t = t;
+
+		if(!usingFunctionalExample) {
+			// create tests
+			for(int i = 0; i < count; ++i) {
+				tests[i] = new ThreadTest(""+(char)('a'+i));
 			}
-		}
-		// wrap tests with a thread
-		for(int i = 0; i < count; ++i)
-		{
-			// "normal" way to thread
-//			threads[i] = new Thread(tests[i]);
-			// more functional programming style
-			threads[i] = new Thread(
-				new temp(tests[i]){
-					public void run(){
-						t.DoTheThing();
-					}
+			// wrap tests with a thread
+			for(int i = 0; i < count; ++i) {
+				// "normal" way to thread
+				threads[i] = new Thread(tests[i]);
+			}
+			// start the threads
+			for(int i = 0; i < count; ++i) {
+				threads[i].start();
+			}
+		} else {
+			abstract class temp implements Runnable{
+				ThreadTest t;
+				public temp(ThreadTest t) {
+					this.t = t;
 				}
-			);
-		}
-		// start the threads
-		for(int i = 0; i < count; ++i)
-		{
-			threads[i].start();
+			}
+			// wrap tests with a thread
+			for(int i = 0; i < count; ++i)
+			{
+				// "normal" way to thread
+				threads[i] = new Thread(tests[i]);
+				// more functional programming style
+				threads[i] = new Thread(
+					new temp(tests[i]){
+						public void run(){
+							t.DoTheThing();
+						}
+					}
+				);
+			}
+			// start the threads
+			for(int i = 0; i < count; ++i)
+			{
+				threads[i].start();
+			}
 		}
 	}
 }
